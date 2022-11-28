558ae2316ce1cdf5af83bed299f9a908bbe6e636
dima-five <dmitriy.pyatibrat@collabio.team>
2022-11-28 21:38:03 +0300

    create fixture

--- conftest.py
+++ conftest.py
@@ -5,21 +5,28 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--browser", default="chrome"
-    )
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--url", default="http://192.168.0.116:8081")
+
 
 @pytest.fixture
 def browser(request):
+    # Getting different params for run 'pytest'
     _browser = request.config.getoption("--browser")
+    _url = request.config.getoption("--url")
     driver = None
-    if _browser == 'firefox' or _browser == 'ff':
-        driver = webdriver.Firefox(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\geckodriver")
-    elif _browser == 'chrome':
+
+    # if _browser == 'firefox' or _browser == 'ff':
+    #     driver = webdriver.Firefox(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\geckodriver")
+    # elif _browser == 'chrome':
+    #     driver = webdriver.Chrome(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\chromedriver")
+    # elif _browser == 'edge':
+    #     driver = webdriver.Edge(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\msedgedriver")
+    # elif _browser == 'yandex':
+    #     driver = webdriver.Chrome(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\yandexdriver")
+    if _browser == 'chrome':
         driver = webdriver.Chrome(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\chromedriver")
-    elif _browser == 'edge':
-        driver = webdriver.Edge(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\msedgedriver")
-    elif _browser == 'yandex':
-        driver = webdriver.Chrome(executable_path="C:\\Users\\fivef\\Downloads\\webdrivers\\yandexdriver")
     yield driver
     driver.close()
+
+
